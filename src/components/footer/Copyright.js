0b72d51e8f61e4352bc1d64c852214b50e78d55f
Mashima <52222646+zimashima@users.noreply.github.com>
2020-06-18 14:20:22 -0400

    added footer and more styling

---
+++ src/components/footer/Copyright.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Copyright() {
+    return (
+        <div className="copyright">
+            Copyright Co-Unity 2020 All rights reserved
+        </div>
+    )
+}
+
+export default Copyright
---
+++ src/components/footer/FooterInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function FooterInfo() {
+    return (
+        <div className="footer-info">
+            <div className="about-team">
+                <h4>About Co-Unity</h4>
+                <p>Meet the Team</p>
+            </div>
+            <div className="connect">
+                <h4>Stay Connected</h4>
+            </div>
+        </div>
+    )
+}
+
+export default FooterInfo
